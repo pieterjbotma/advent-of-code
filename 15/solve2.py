bd1129daab39163df9302a5098b287412ec41c43
Pieter Botma <P.J.Botma-1@tudelft.nl>
2021-12-23 04:22:37 +0100

    solved day 15

--- 15/solve2.py
+++ 15/solve2.py
@@ -1,15 +1,8 @@
 from functools import cache
 from time import time_ns
-import sys
-import gc
-from collections import defaultdict
-from bisect import bisect
 import heapq
 
-H = 3
-sys.setrecursionlimit(int(1e4))
-
-grid = tuple(tuple(map(int,line.strip())) for line in open('example.txt'))
+grid = tuple(tuple(map(int,line.strip())) for line in open('input.txt'))
 Y = len(grid)
 X = len(grid[0])
 
@@ -17,28 +10,23 @@ X = len(grid[0])
 def expand(point):
     return [[(point+x+y-1)%9+1 for x in range(5)] for y in range(5)]
 
-# grid = [[expand(grid[y%Y][x%X])[int(y/Y)][int(x/X)] for x in range(5*X)] for y in range(5*Y)]
-# Y = len(grid)
-# X = len(grid[0])
+grid = [[expand(grid[y%Y][x%X])[int(y/Y)][int(x/X)] for x in range(5*X)] for y in range(5*Y)]
+Y = len(grid)
+X = len(grid[0])
 
-min_risk = [[0 for x in range(X)] for y in range(Y)] 
-
-x=0
-y=0
-
+@cache
 def neighbours(x,y):
     return ((nx,ny) for dx,dy in [(0,-1),(0,1),(-1,0),(1,0)] if 0 <= (nx:=x+dx) < X and 0 <= (ny:=y+dy) < Y)
 
-print(neighbours)
+visited = set()
+points_to_check = [(0,0,0)]
 
-for gridline in min_risk:
-    print(gridline)
-
-paths = [(0,(0,0))]
-
-while paths:
-    
-
-for neighbour in neighbours():
-
-heapq.heappush(neighbour for neighbour in neighbours)
+while points_to_check:
+    risk,x,y = heapq.heappop(points_to_check)
+    if (x,y) == (X-1,Y-1):
+        print(risk)
+        break
+    for nx,ny in neighbours(x,y):
+        if (nx,ny) not in visited:
+            heapq.heappush(points_to_check,(risk+grid[ny][nx],nx,ny))
+    visited.add((x,y))
